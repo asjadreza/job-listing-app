da0a6bea90a3789cf0113f1a6c3f98854d902699
asjadreza <mdasjad.reza@symbtechnologies.com>
2024-09-19 14:48:16 +0530

    created job listing app

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import Homecards from "@/components/Homecards.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
+<template>
+  <Hero
+    title="Become a Vue Dev"
+    subTitle="Find the Vue job that fits your skills and needs"
+  />
+  <Homecards />
+  <JobListings :limit="3" :showButton="true" />
+</template>
